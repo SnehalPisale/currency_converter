a242bebef07204716a8cc6f4d8db4aeb7db00ed8
SnehalPisale <snehalpisale409@gmail.com>
2025-12-31 00:30:13 -0800

    Second commit

--- currency_converter.sh
+++ currency_converter.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
-# Fixed conversion rate (example: 1 USD = 83 INR)
-rate=83
-
-# Ask user for amount in INR
+rate=85
 echo "Enter amount in INR:"
 read inr
-
-# Convert INR to USD
 usd=$(echo "scale=2; $inr / $rate" | bc)
-
-# Display result
 echo "INR $inr = USD $usd"
+
